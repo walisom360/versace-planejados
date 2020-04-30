61697500d65aa1964651040fae90af6c833a88e8
walisonmatheus <walisom360@hotmail.com>
2020-04-30 13:07:36 -0300

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi Walison </h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Em breve</h1>
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
